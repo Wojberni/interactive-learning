41306e2aedbdcf40c5fbd1defb1e8bebbc354b7d
Maciej Bartczak <maciekbartczak17@gmail.com>
2022-10-25 18:49:39 +0200

    added email to user registration

--- backend/domain/domain-api/src/main/kotlin/com/zam/interactivelearning/domain/api/user/CreateUserCommand.kt
+++ backend/domain/domain-api/src/main/kotlin/com/zam/interactivelearning/domain/api/user/CreateUserCommand.kt
@@ -3,6 +3,7 @@ package com.zam.interactivelearning.domain.api.user
 import com.zam.interactivelearning.cqrs.Command
 
 data class CreateUserCommand(
+    val email: String,
     val username: String,
     val password: String,
 ): Command<Unit> {
