5368c89a08049a2860504eaa3017ab83f0804f72
Wojberni <wojciech.bernatek@gmail.com>
2022-10-28 20:28:48 +0200

    Added flashcard, quiz, question and answer entities and fixed flyway migration

---
+++ backend/domain/domain-application/src/main/kotlin/com/zam/interactivelearning/domain/application/quiz/AnswerEntity.kt
@@ -0,0 +1,18 @@
+package com.zam.interactivelearning.domain.application.quiz
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "ANSWERS")
+class AnswerEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "answers_id_seq")
+    @SequenceGenerator(name = "answers_id_seq", sequenceName = "answers_id_seq", allocationSize = 1)
+    var id: Long = 0,
+    var content: String = "",
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    var question: QuestionEntity? = null,
+    var isCorrect: Boolean = false,
+    var createdBy: Long = 0
+)
