1c4691b39f82224d4043d3f3fe7c0203b7ff9a8d
maciekbartczak <maciekbartczak17@gmail.com>
2022-11-20 22:59:19 +0100

    added validation hints

--- backend/infrastructure/infrastructure-api/src/main/kotlin/com/zam/interactivelearning/infrastructure/api/delivery/friends/FriendRequest.kt
+++ backend/infrastructure/infrastructure-api/src/main/kotlin/com/zam/interactivelearning/infrastructure/api/delivery/friends/FriendRequest.kt
@@ -1,6 +1,10 @@
 package com.zam.interactivelearning.infrastructure.api.delivery.friends
 
+import javax.validation.constraints.NotNull
+
 data class FriendRequest(
+    @get: NotNull
     val id: Long,
+    @get: NotNull
     val senderUsername: String,
 )
