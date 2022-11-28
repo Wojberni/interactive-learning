f30f0a042095c10cdac9ec3b4b2c412dca59d606
Maciej Bartczak <maciekbartczak17@gmail.com>
2022-11-28 17:49:47 +0100

    notifications - mobile

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,20 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:mobile/router/custom_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:learning_api/api.dart';
+import 'package:mobile/router/custom_router.dart';
 
 import 'api/ApiClient.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken() ?? '';
+
+
   String initialRoute = await getInitialRoute();
+  if (initialRoute == '/home') {
+      NotificationsEndpointApi(apiClient).registerOrUpdateDeviceToken(
+          RegisterOrUpdateDeviceTokenRequest(token: fcmToken)
+      );
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
-  runApp(MyApp(initialRoute: initialRoute));
-      });
+    runApp(MyApp(initialRoute: initialRoute));
+  });
 }
 
 class MyApp extends StatelessWidget {
