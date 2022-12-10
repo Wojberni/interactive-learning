0657527940038e09d9221fcedc826136a8d79988
maciekbartczak <maciekbartczak17@gmail.com>
2022-12-10 23:04:40 +0100

    fix daily challenge rotation

--- backend/domain/domain-application/src/main/kotlin/com/zam/interactivelearning/domain/application/quiz/QuizRepository.kt
+++ backend/domain/domain-application/src/main/kotlin/com/zam/interactivelearning/domain/application/quiz/QuizRepository.kt
@@ -1,6 +1,9 @@
 package com.zam.interactivelearning.domain.application.quiz
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface QuizRepository: JpaRepository<QuizEntity, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM quiz ORDER BY RANDOM() LIMIT 1")
+    fun getRandomQuiz(): QuizEntity
 }
