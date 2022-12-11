b13534dbe89e76880305ae8a9a957ac4112d692d
maciekbartczak <maciekbartczak17@gmail.com>
2022-12-11 19:21:28 +0100

    handle empty search query

--- backend/domain/domain-application/src/main/kotlin/com/zam/interactivelearning/domain/application/search/SearchActivityQueryHandler.kt
+++ backend/domain/domain-application/src/main/kotlin/com/zam/interactivelearning/domain/application/search/SearchActivityQueryHandler.kt
@@ -4,7 +4,9 @@ import com.zam.interactivelearning.cqrs.QueryHandler
 import com.zam.interactivelearning.domain.api.search.ActivityKind
 import com.zam.interactivelearning.domain.api.search.SearchActivityQuery
 import com.zam.interactivelearning.domain.api.search.SearchActivityResult
+import com.zam.interactivelearning.domain.application.flashcard.FlashCardEntity
 import com.zam.interactivelearning.domain.application.flashcard.FlashcardRepository
+import com.zam.interactivelearning.domain.application.quiz.QuizEntity
 import com.zam.interactivelearning.domain.application.quiz.QuizRepository
 import kotlin.reflect.KClass
 
@@ -14,14 +16,30 @@ class SearchActivityQueryHandler(
 ): QueryHandler<SearchActivityQuery, List<SearchActivityResult>> {
 
     override fun handle(query: SearchActivityQuery): List<SearchActivityResult> {
+        if (query.query.isBlank()) {
+            return quizRepository.findAll()
+                .map { mapQuiz(it) }
+                .plus(
+                    flashcardRepository.findAll()
+                    .map { mapFlashcard(it) }
+                )
+        }
         val quizzes = quizRepository.findALlByNameContainsIgnoreCaseOrDescriptionContainsIgnoreCase(query.query, query.query)
-            .map { SearchActivityResult(it.id, it.name, it.description, ActivityKind.QUIZ) }
+            .map { mapQuiz(it)}
         val flashcards = flashcardRepository.findAllByContentContainsIgnoreCase(query.query)
-            .map { SearchActivityResult(it.id, it.content, "", ActivityKind.FLASHCARD) }
+            .map { mapFlashcard(it) }
         return quizzes + flashcards
     }
 
     override fun supportedClass(): KClass<SearchActivityQuery> {
         return SearchActivityQuery::class
+    }
+
+    private fun mapQuiz(quiz: QuizEntity): SearchActivityResult {
+        return SearchActivityResult(quiz.id, quiz.name, quiz.description, ActivityKind.QUIZ)
+    }
+
+    private fun mapFlashcard(flashcard: FlashCardEntity): SearchActivityResult {
+        return SearchActivityResult(flashcard.id, flashcard.content, "", ActivityKind.FLASHCARD)
     }
 }
