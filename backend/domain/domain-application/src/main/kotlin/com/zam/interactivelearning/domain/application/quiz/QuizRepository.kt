df1fced02ce0902c6e89cb0c58983e714f2e26cc
maciekbartczak <maciekbartczak17@gmail.com>
2022-12-18 12:10:18 +0100

    get random quiz endpoint

--- backend/domain/domain-application/src/main/kotlin/com/zam/interactivelearning/domain/application/quiz/QuizRepository.kt
+++ backend/domain/domain-application/src/main/kotlin/com/zam/interactivelearning/domain/application/quiz/QuizRepository.kt
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.Query
 
 interface QuizRepository: JpaRepository<QuizEntity, Long> {
     @Query(nativeQuery = true, value = "SELECT * FROM quizzes WHERE id not in (SELECT id FROM daily_challenge) ORDER BY RANDOM() LIMIT 1;")
+    fun getNextDailyChallenge(): QuizEntity
+
+    @Query(nativeQuery = true, value = "SELECT * FROM quizzes ORDER BY RANDOM() LIMIT 1;")
     fun getRandomQuiz(): QuizEntity
 
     fun findALlByNameContainsIgnoreCaseOrDescriptionContainsIgnoreCase(name: String, description: String): List<QuizEntity>
